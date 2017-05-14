db954359d7f62f22477423befbc9bf46de3bcbad
fitblip <fitblip@gmail.com>
2017-05-13 22:07:09 -0700

    Add screenshots, readme, and setup.py

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Lumberjack
+<p align="center">
+
+    <img style="width: 300px" src="https://github.com/CaliDog/Lumberjack/raw/master/img/logo.png">
+    <p align="center">Harvester of certificates, bearer of flannel.</p>
+</p>
+
+## What?
+Axeman is a utility for downloading, parsing, and storing <a href="">Certificate Transparency Lists</a> using python3's concurrency and multi-processing. Its aim is to download and parse certificates relatively quickly and efficiently, storing them in CSVs on the local filesystem. 
+
+## Installing it
+Installation should be super straight forward, but you need a newer version of python (3.5+) to run it.
+
+```
+pip3 install axeman
+```
+
+## Usage
+<img src="https://github.com/CaliDog/Lumberjack/raw/master/img/screenshot.png">
+
+## Demo
+This is axeman running in debug mode and downloading only the "skydiver" log. 
+
+<img src="http://i.imgur.com/4JEME7j.gif">
---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+import os
+import sys
+
+import axeman
+
+py_version = sys.version_info[:2]
+
+if py_version < (3, 5):
+    raise Exception("axeman requires Python >= 3.5.")
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open('requirements.txt') as f:
+    dependencies = f.read().splitlines()
+
+long_description = """
+Axeman uses co-routines and multiprocessing to download and process certificates from a Certificate Transparency
+List and outputs them to CSV.
+"""
+
+setup(
+    name='axeman',
+    version=axeman.__version__,
+    url='https://github.com/CaliDog/Axeman/',
+    author='Ryan Sears',
+    install_requires=dependencies,
+    author_email='ryan@calidog.io',
+    description='Lumberjack is a multi-threaded and concurrent certificate transparency retriever Edit',
+    long_description=long_description,
+    packages=['axeman'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'axeman = axeman:main',
+        ],
+    },
+    license = "MIT",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Testing",
+        "Environment :: Console",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
